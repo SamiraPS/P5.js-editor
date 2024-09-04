4e8d7685b0ccd34156d7f1050573e776fc778cf0
SamiraPS <168238782+SamiraPS@users.noreply.github.com>
2024-09-04 17:04:18 -0300

    Update skecth.js

--- skecth.js
+++ skecth.js
@@ -1,7 +1,13 @@
 function setup() {
   createCanvas(400, 400);
+  background("green");
+ 
 }
-
+  
 function draw() {
-  background("black") (220);
+  if(mouseIsPressed){}
+  stroke("yellow");//COR DA LINHA
+  fill("blue"); //cor do fundo
+  rect(mouseX,mouseY,30,30);//criande o retângulo
+
 }
